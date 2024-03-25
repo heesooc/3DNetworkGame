32006a480e1d368151e3d90a8b24695817b90c36
조희수 <hs.jo.sw.p@gmail.com>
2024-03-25 16:50:11 +0900

    실습 과제 1. 캐릭터 이동 기능 구현 실습 과제 2. 캐릭터 및 카메라 회전 기능 구현 (3인칭, 시네머신: Virtual Camera 사용) 실습 과제 3. 블렌드 트리를 이용해서 걷기와 뛰기가 있는 애니메이터를 만들어 적용하세요.

---
+++ Assets/02.Scripts/Character/CharacterMoveAbility.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))] // rigidbody 쓰지 않고 가능하다는 뜻
+[RequireComponent(typeof(Animator))]
+
+public class CharacterMoveAbility : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private Animator _animator;
+
+    public float MoveSpeed = 2f;
+    private float VerticalSpeed = 0f;
+    private float _gravity = -9.8f;
+
+    // 목표: [W], [A], [S], [D] 및 방향키를 누르면 캐릭터를 그 방향으로 이동시키고 싶다.
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // 순서
+        // 1. 사용자의 키보드 입력을 받는다.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // 2. "캐릭터가 바라보는 방향을 기준으로 방향을 설정한다.
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();
+        dir = Camera.main.transform.TransformDirection(dir);
+
+
+        _animator.SetFloat("Move", dir.magnitude);
+
+        // 4. 중력 적용하세요.
+        if (!_characterController.isGrounded)
+        {
+            VerticalSpeed += _gravity * Time.deltaTime;
+        }
+        else
+        {
+            VerticalSpeed = 0; // 지면에 닿아 있을 경우 수직 속도를 0으로 리셋
+        }
+
+
+        // 3. 이동속도에 따라 그 방향으로 이동한다. 
+        dir.y = VerticalSpeed;
+        _characterController.Move(dir * MoveSpeed * Time.deltaTime);
+
+
+    }
+}
---
+++ Assets/02.Scripts/Character/CharacterRotateAbility.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotateAbility : MonoBehaviour
+{
+    // 목표: 마우스 이동에 따라 카메라와 플레이어를 회전하고 싶다.
+    public Transform CameraRoot;
+
+    public float RotationSpeed = 200f;
+    private float _mx; // 각도
+    private float _my;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void Update()
+    {
+        // 순서:
+        // 1. 마우스 입력 값을 받는다.
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // 2. 회전 값을 마우스 입력에 따라 미리 누적한다.
+        _mx += mouseX * RotationSpeed * Time.deltaTime;
+        _my += mouseY * RotationSpeed * Time.deltaTime;
+        _my = Mathf.Clamp(_my, -90f, 90f);
+
+        // 3. 카메라(3인칭)와 캐릭터를 회전 방향으로 회전시킨다. 
+        transform.eulerAngles = new Vector3(0, _mx, 0f); // X와 Z 축은 0으로 고정, 회전은 Y축을 중심으로만 
+        CameraRoot.localEulerAngles = new Vector3(-_my, 0, 0f); // "로컬"은 부모 오브젝트의 회전을 기준으로 한다
+                                                                // -_my: 마우스를 위로 움직일 때 카메라가 아래를 향하게 
+
+        // 4. 시네머신- virtual 카메라
+
+    }
+}
