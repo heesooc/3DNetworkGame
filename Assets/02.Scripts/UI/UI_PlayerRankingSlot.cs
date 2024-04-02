cbe0828dd0f4f8714f842a936175fba43443874e
조희수 <hs.jo.sw.p@gmail.com>
2024-04-02 17:35:00 +0900

    verticalLayoutGroup 사용하여 랭킹 표기

---
+++ Assets/02.Scripts/UI/UI_PlayerRankingSlot.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[SerializeField]
+public class UI_PlayerRankingSlot : MonoBehaviour
+{
+    public Text RankingTextUI;
+    public Text NicknameTextUI;
+    public Text KillCountTextUI;
+    public Text ScoreTextUI;
+}
