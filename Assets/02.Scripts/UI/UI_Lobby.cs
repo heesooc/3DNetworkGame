3128063b369dc06b0f3573dd61a7a0a781fc330d
조희수 <hs.jo.sw.p@gmail.com>
2024-04-09 16:47:17 +0900

    룸 커스텀 프로퍼티

--- Assets/02.Scripts/UI/UI_Lobby.cs
+++ Assets/02.Scripts/UI/UI_Lobby.cs
@@ -1,9 +1,8 @@
 using Photon.Pun;
 using Photon.Realtime;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public enum CharacterType
 {
@@ -41,11 +40,20 @@ public class UI_Lobby : MonoBehaviour
 
         PhotonNetwork.NickName = nickname;
 
+        // [룸 옵션 설정]
         RoomOptions roomOptions = new RoomOptions();
          roomOptions.MaxPlayers = 20;    // 입장 가능한 최대 플레이어 수
          roomOptions.IsVisible = true;   // 로비에서 방 목록에 노출할 것인가?
          roomOptions.IsOpen = true;      // 방에 다른 플레이어가 들어올 수 있는가?
         roomOptions.EmptyRoomTtl = 1000 * 20; // 비어 있는 방이 얼마나 타임 투 리브(Time to live, TTL): 컴퓨터나 네트워크에서 데이터의 유효 기간을 나타내기 위한 방법
+        roomOptions.CustomRoomProperties = new Hashtable() // 룸 커스텀 프로퍼티 (플레이어 커스텀 프로피터)
+        {
+            {"MasterNickname", nickname}
+        };
+        // 로비에서 공개적으로 표시될 룸 커스텀 프로퍼티의 키를 정의
+        // -> 방을 검색하거나 선택할 때 사용자에게 유용한 정보를 제공하기 위해 사용
+        roomOptions.CustomRoomPropertiesForLobby = new string[] { "MasterNickname" };
+
 
         PhotonNetwork.JoinOrCreateRoom(roomID, roomOptions, TypedLobby.Default); // 방이 있다면 입장하고 없다면 만드는 것
     }
@@ -59,5 +67,16 @@ public class UI_Lobby : MonoBehaviour
         SelectedCharacterType = characterType;
         FemaleCharacter.SetActive(SelectedCharacterType == CharacterType.Female);
         MaleCharacter.SetActive(SelectedCharacterType == CharacterType.Male);
+    }
+
+
+    public void OnNicknameValueChanged(string newValue)
+    {
+        if (string.IsNullOrEmpty(newValue)) 
+        {
+            return;
+        }
+
+        PhotonNetwork.NickName = newValue;
     }
 }
